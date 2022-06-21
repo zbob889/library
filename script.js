a9bfb1dba86674b8344b7f056d963f8c13ae7c3e
zbob889 <zroberts889@gmail.com>
2022-06-21 15:45:44 +0200

    Got add book button working

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let myLibrary = [];
 let libraryTable = document.getElementById('library');
+let bookButton = document.getElementById('button');
 
 function Book(title, author, pages, status) {
     this.title = title;
@@ -11,7 +12,7 @@ function Book(title, author, pages, status) {
 function addBookToLibrary (title, author, pages, status) {
     let book = new Book(title, author, pages, status);
     myLibrary.push(book);
-}
+};
 
 function displayLibrary(myLibrary) {
     for(let i = 0; i < myLibrary.length; i++){
@@ -25,4 +26,28 @@ function displayLibrary(myLibrary) {
         pages.innerHTML = `${myLibrary[i].pages}`;
         status.innerHTML = `${myLibrary[i].status}`;
     };
-}
+};
+
+bookButton.addEventListener('click', () => {
+    let title = document.getElementById('formTitle');
+    let author = document.getElementById('formAuthor');
+    let pages = document.getElementById('formPages');
+    let status = document.getElementById('formStatus');
+
+    addBookToLibrary(title.value, author.value, pages.value, status.value);
+
+    let row = libraryTable.insertRow(myLibrary.length);
+    let titleCell = row.insertCell(0);
+    let authorCell = row.insertCell(1);
+    let pagesCell = row.insertCell(2);
+    let statusCell = row.insertCell(3);
+    titleCell.innerHTML = `${myLibrary[myLibrary.length - 1].title}`;
+    authorCell.innerHTML = `${myLibrary[myLibrary.length - 1].author}`;
+    pagesCell.innerHTML = `${myLibrary[myLibrary.length - 1].pages}`;
+    statusCell.innerHTML = `${myLibrary[myLibrary.length - 1].status}`;
+
+    title.value = '';
+    author.value = '';
+    pages.value = '';
+    status.value = '';
+});
